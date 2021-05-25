fce8bdc5f29dc1d7e6cd325ae63b94b1dd138cdb
Raquel Maria <raquel.silva1@outlook.com.br>
2021-05-25 17:12:35 -0300

    Added card view and view binding

--- app/src/main/java/com/ramaria/chopnboil/MainActivity.kt
+++ app/src/main/java/com/ramaria/chopnboil/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ramaria.chopnboil
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -7,16 +8,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.navigation.NavigationView
+import com.ramaria.chopnboil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+   private lateinit var binding: ActivityMainBinding
    private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
@@ -27,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
