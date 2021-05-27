8175e069770adeef08bca9865ceeecc91804c333
Raquel Maria <raquel.silva1@outlook.com.br>
2021-05-27 15:18:03 -0300

    Genereted model classes

---
+++ app/src/main/java/com/ramaria/chopnboil/model/Result.kt
@@ -0,0 +1,35 @@
+package com.ramaria.chopnboil.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("aggregateLikes")
+    val aggregateLikes: Int,
+    @SerializedName("cheap")
+    val cheap: Boolean,
+    @SerializedName("dairyFree")
+    val dairyFree: Boolean,
+    @SerializedName("ExtendedIngredients")
+    val extendedIngredients: List<ExtendedIngredients>,
+    @SerializedName("glutenFree")
+    val glutenFree: Boolean,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("readyInMinutes")
+    val readyInMinutes: Int,
+    @SerializedName("sourceName")
+    val sourceName: String,
+    @SerializedName("sourceUrl")
+    val sourceUrl: String,
+    @SerializedName("summary")
+    val summary: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("vegan")
+    val vegan: Boolean,
+    @SerializedName("vegetarian")
+    val vegetarian: Boolean
+)
